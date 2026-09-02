957ade1e6c178accc884bd9901f74746a4446223
Al Ramos <al.ramos777@gmail.com>
2026-09-02 15:10:38 -0400

    added icons; switched to .env use(oops)

--- movie-watchlist/src/components/Search.jsx
+++ movie-watchlist/src/components/Search.jsx
@@ -1,20 +1,25 @@
-import React, { useState } from "react";
+import React, { useState } from 'react';
 
 export default function Search({ query, handleChange, handleSearch }) {
-  return (
-    <div className="absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white w-[75vw] flex border-2 border-gray-400 rounded-md">
-      <input
-        placeholder="Search for a movie"
-        onChange={handleChange}
-        value={query}
-        className="p-3 w-5/6"
-      />
-      <button
-        className="self-stretch bg-gray-200 w-1/6 inline-block transition duration-300 hover:bg-gray-300 cursor-pointer"
-        onClick={handleSearch}
-      >
-        Search
-      </button>
-    </div>
-  );
+	return (
+		<div className='absolute top-full left-1/2 -translate-x-1/2 -translate-y-1/2 bg-white w-[75vw] flex border-2 border-gray-400 rounded-md'>
+			<input
+				placeholder='Search for a movie'
+				onChange={handleChange}
+				onKeyDown={(e) => {
+					if (e.key === 'Enter') {
+						handleSearch();
+					}
+				}}
+				value={query}
+				className='p-3 w-5/6'
+			/>
+			<button
+				className='self-stretch bg-gray-200 w-1/6 inline-block transition duration-300 hover:bg-gray-300 cursor-pointer'
+				onClick={handleSearch}
+			>
+				Search
+			</button>
+		</div>
+	);
 }
--- movie-watchlist/src/script.jsx
+++ movie-watchlist/src/script.jsx
@@ -1,101 +1,104 @@
-import React, { useState, useEffect } from "react";
-import ReactDOM from "react-dom/client";
-import Header from "./components/Header.jsx";
-import Search from "./components/Search.jsx";
-import Movie from "./components/Movie.jsx";
-import emptyStateIcon from "./assets/images/Icon.svg";
+import React, { useState, useEffect } from 'react';
+import ReactDOM from 'react-dom/client';
+import Header from './components/Header.jsx';
+import Search from './components/Search.jsx';
+import Movie from './components/Movie.jsx';
+import emptyStateIcon from './assets/images/reel.svg';
 
-const apiKey = "34dd9606";
+const apiKey = import.meta.env.VITE_OMDB_API_KEY;
 const apiUrl = `https://www.omdbapi.com/?apikey=${apiKey}&`;
 
 function App() {
-  const [isHome, setHome] = useState(true);
-  const [movielist, setMovielist] = useState([]);
-  const [moviedetails, setMoviedetails] = useState([]);
-  const [watchlist, setWatchlist] = useState([]);
-  const [query, setQuery] = useState("");
+	const [isHome, setHome] = useState(true);
+	const [movielist, setMovielist] = useState([]);
+	const [moviedetails, setMoviedetails] = useState([]);
+	const [watchlist, setWatchlist] = useState([]);
+	const [query, setQuery] = useState('');
 
-  useEffect(() => {
-    if (!movielist || movielist.length === 0) return;
+	useEffect(() => {
+		if (!movielist || movielist.length === 0) return;
 
-    const fetchDetails = async () => {
-      const results = await Promise.all(
-        movielist.map(async (movie) => {
-          const response = await fetch(`${apiUrl}i=${movie.imdbID}`);
-          return response.json();
-        })
-      );
+		const fetchDetails = async () => {
+			const results = await Promise.all(
+				movielist.map(async (movie) => {
+					const response = await fetch(`${apiUrl}i=${movie.imdbID}`);
+					return response.json();
+				}),
+			);
 
-      setMoviedetails(results);
-    };
+			setMoviedetails(results);
+		};
 
-    fetchDetails();
-  }, [movielist]);
+		fetchDetails();
+	}, [movielist]);
 
-  const handleSearch = async () => {
-    const url = `${apiUrl}s=${query}`;
+	const handleSearch = async () => {
+		const url = `${apiUrl}s=${query}`;
 
-    try {
-      const response = await fetch(url);
-      const data = await response.json();
+		try {
+			const response = await fetch(url);
+			const data = await response.json();
 
-      setMovielist(data.Search);
-    } catch (error) {
-      console.log(error);
-    }
-  };
+			setMovielist(data.Search);
+		} catch (error) {
+			console.log(error);
+		}
+	};
 
-  const handleChange = (evt) => {
-    setQuery(evt.target.value);
-  };
+	const handleChange = (evt) => {
+		setQuery(evt.target.value);
+	};
 
-  const toggleWatchlist = (id) => {
-    console.log(id);
-    if (watchlist.indexOf(id) > -1) {
-      setWatchlist(watchlist.filter((item) => !id));
-    } else {
-      setWatchlist([...watchlist, id]);
-    }
-  };
+	const toggleWatchlist = (id) => {
+		console.log(id);
+		if (watchlist.indexOf(id) > -1) {
+			setWatchlist(watchlist.filter((item) => !id));
+		} else {
+			setWatchlist([...watchlist, id]);
+		}
+	};
 
-  return (
-    <div className="flex flex-col h-screen">
-      <div className="h-[33vh] relative">
-        <Header isHome={isHome} pageSwitch={() => setHome(!isHome)}></Header>
-        {isHome ? (
-          <Search
-            query={query}
-            handleChange={handleChange}
-            handleSearch={handleSearch}
-          ></Search>
-        ) : (
-          ""
-        )}
-      </div>
+	return (
+		<div className='flex flex-col h-screen'>
+			<div className='h-[33vh] relative'>
+				<Header
+					isHome={isHome}
+					pageSwitch={() => setHome(!isHome)}
+				></Header>
+				{isHome ? (
+					<Search
+						query={query}
+						handleChange={handleChange}
+						handleSearch={handleSearch}
+					></Search>
+				) : (
+					''
+				)}
+			</div>
 
-      <main className="h-[67vh]">
-        <div className="w-[85vw] h-full mx-auto py-8">
-          {moviedetails.length == 0 ? (
-            <div className="h-full flex-col flex items-center justify-center text-[#DFDDDD] font-bold space-y-2">
-              <img src={emptyStateIcon} alt="" />
-              <span>Start Exploring</span>
-            </div>
-          ) : (
-            moviedetails.map((movie) => (
-              <Movie
-                key={movie.imdbID}
-                movie={movie}
-                watchlist={watchlist}
-                toggleWatchlist={toggleWatchlist}
-              ></Movie>
-            ))
-          )}
-        </div>
-      </main>
-    </div>
-  );
+			<main className='h-[67vh]'>
+				<div className='w-[85vw] h-full mx-auto py-8'>
+					{moviedetails.length == 0 ? (
+						<div className='h-full flex-col flex items-center justify-center text-[#DFDDDD] font-bold space-y-2'>
+							<img src={emptyStateIcon} alt='' />
+							<span>Start Exploring</span>
+						</div>
+					) : (
+						moviedetails.map((movie) => (
+							<Movie
+								key={movie.imdbID}
+								movie={movie}
+								watchlist={watchlist}
+								toggleWatchlist={toggleWatchlist}
+							></Movie>
+						))
+					)}
+				</div>
+			</main>
+		</div>
+	);
 }
 
-const root = ReactDOM.createRoot(document.getElementById("app"));
+const root = ReactDOM.createRoot(document.getElementById('app'));
 
 root.render(<App />);
